fd1369294fa86996ecdd4b05b169799471bf9c68
Rafal Szymanski <r.szymansky@gmail.com>
2020-05-21 21:58:51 +0200

    Implement users, staff, admin access

--- medok/users/models.py
+++ medok/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core import validators
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
@@ -8,10 +9,16 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = models.CharField(_("Name"), null=False, blank=False, max_length=255,)
-    surname = models.CharField(_("Surname"), null=False, blank=False, max_length=255,)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
     pesel = models.BigIntegerField(
-        _("PESEL"), null=False, blank=False, min_length=11, max_length=11,
+        _("PESEL"),
+        null=False,
+        blank=False,
+        default=11223344555,
+        validators=[
+            validators.MinValueValidator(10000000000),
+            validators.MaxValueValidator(99999999999),
+        ],
     )
     nurse = models.BooleanField(_("Nurse"), default=True)
     doctor = models.BooleanField(_("Doctor"), default=False)
@@ -20,6 +27,7 @@ class User(AbstractUser):
     occupational_therapist = models.BooleanField(
         _("Occupational therapist"), default=False
     )
+    superuser = models.BooleanField(_("Superuser"), default=False)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
