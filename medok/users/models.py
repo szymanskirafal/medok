7b360058afbabe23504e1a1153221655af7c45fd
Rafal Szymanski <r.szymansky@gmail.com>
2020-05-21 20:14:04 +0200

    Add fields to User model

--- medok/users/models.py
+++ medok/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,7 +8,16 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name"), null=False, blank=False, max_length=255,)
+    surname = models.CharField(_("Surname"), null=False, blank=False, max_length=255,)
+    pesel = models.PositiveIntegerField(_("PESEL"), null=False, blank=False)
+    nurse = models.BooleanField(_("Nurse"), default=True)
+    doctor = models.BooleanField(_("Doctor"), default=False)
+    psychologist = models.BooleanField(_("Psychologist"), default=False)
+    physiotherapist = models.BooleanField(_("Physiotherapist"), default=False)
+    occupational_therapist = models.BooleanField(
+        _("Occupational therapist"), default=False
+    )
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
