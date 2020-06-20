19993de1c0162a676a1d3c5ea5b6be470c77af37
Rafal Szymanski <r.szymansky@gmail.com>
2020-06-20 08:54:27 +0200

    Polish forms

--- medok/patients/models.py
+++ medok/patients/models.py
@@ -6,8 +6,8 @@ from django.utils.translation import ugettext_lazy as _
 class Patient(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    name = models.CharField(_("Imię"), blank=False, max_length=50)
-    surname = models.CharField(_("Nazwisko"), blank=False, max_length=100)
+    name = models.CharField(_("Imię"), max_length=50)
+    surname = models.CharField(_("Nazwisko"), max_length=100)
     pesel = models.BigIntegerField(
         _("PESEL"),
         null=False,
@@ -18,16 +18,14 @@ class Patient(models.Model):
             validators.MaxValueValidator(99999999999),
         ],
     )
-    street = models.CharField(_("Ulica"), blank=True, max_length=100)
-    nr = models.PositiveSmallIntegerField(_("Numer"), blank=True)
-    zip_code = models.CharField(_("Kod pocztowy"), blank=True, max_length=6)
-    city = models.CharField(_("Miejscowość"), blank=True, max_length=100)
-    birthday = models.DateField(_("Data urodzenia"), blank=True,)
-    nr_in_main_book = models.CharField(
-        _("Numer w Książce Głównej"), blank=True, max_length=100,
-    )
+    street = models.CharField(_("Ulica"), max_length=100)
+    nr = models.PositiveSmallIntegerField(_("Numer"),)
+    zip_code = models.CharField(_("Kod pocztowy"), max_length=6)
+    city = models.CharField(_("Miejscowość"), max_length=100)
+    birthday = models.DateField(_("Data urodzenia"))
+    nr_in_main_book = models.CharField(_("Numer w Książce Głównej"), max_length=100,)
     nr_in_ward_book = models.CharField(
-        _("Numer w Książce Oddziałowej"), blank=True, max_length=100,
+        _("Numer w Książce Oddziałowej"), max_length=100,
     )
     agreed_to_tests = models.BooleanField(_("Zgoda na wykonanie badań"), default=False)
 
